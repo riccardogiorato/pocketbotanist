bd8df79928655e4a9cc32be6a6f635a61c774ae6
giorat <giorat.96@gmail.com>
2018-06-06 10:25:17 +0200

    new model with waterlily

--- js/src/classes.js
+++ js/src/classes.js
@@ -1,7 +1,8 @@
-export const IMAGE_CLASSES ={
-0:    'daisy',
-1:    'dandelion',
-2:    'rose',
-3:    'sunflower',
-4:    'tulip',
+export const IMAGE_CLASSES = {
+    0: 'daisy',
+    1: 'dandelion',
+    2: 'rose',
+    3: 'sunflower',
+    4: 'tulip',
+    5: 'waterlily'
 };
