fe232023f4455956cd6aa65e1c6cef29110696fe
giorat <giorat.96@gmail.com>
2018-06-11 10:35:16 +0200

    updates tensorflowjs + tfjs model

--- js/src/classes.js
+++ js/src/classes.js
@@ -1,8 +1,9 @@
 export const IMAGE_CLASSES = {
     0: 'daisy',
     1: 'dandelion',
-    2: 'rose',
-    3: 'sunflower',
-    4: 'tulip',
-    5: 'waterlily'
+    2: 'iris',
+    3: 'rose',
+    4: 'sunflower',
+    5: 'tulip',
+    6: 'waterlily'
 };
